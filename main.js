7a3414642ad9a1d06eb597f1620b7292827eec9f
Samantha Taylor <staylor@mort.is>
2020-06-15 14:11:08 -0400

    Copied over the 10 katas already done.

---
+++ main.js
@@ -0,0 +1,83 @@
+//Group Project Team Members are: Mike Reams, Joe Langston, Katrine Jackson
+
+
+
+//Kata 1//
+let counter1 = 1
+  while (counter1 <= 20) {
+    console.log(counter1)
+    counter1 += 1
+  }
+
+  //Kata 2//
+  let counter2 = 1
+  while (counter2 <= 20) {
+    if (counter2 % 2 == 0) {
+    console.log(counter2)
+  }
+    counter2 += 1
+  }
+
+  //kata 3//
+  let counter3 = 1
+  while (counter3 <= 20) {
+    console.log(counter3)
+    counter3 += 2
+  }
+
+  //kata 4//
+  let counter4 = 5
+  while (counter4 <= 100) {
+    console.log(counter4)
+    counter4 +=5 
+  }
+
+  //kata 5//
+  let counter5 = 1
+  while (counter5 <= 100) {
+    let isSquare = Number.isInteger(Math.sqrt(counter5));
+    if (isSquare) {
+    console.log(counter5)
+    }
+    counter5 += 1 
+  }
+
+  //kata 6//
+  let counter6 = 20
+  while (counter6 >= 1) {
+    console.log(counter6)
+    counter6 -=1
+  }
+
+  //kata 7//
+  let counter7 = 20
+  while (counter7 >= 1) {
+    if (counter7 % 2 == 0) {
+      console.log(counter7)
+    }
+    counter7 -=1
+  }
+
+  //kata 8//
+  let counter8 = 19
+  while (counter8 >= 1) {
+      console.log(counter8)
+    counter8 -=2
+  }
+
+  //kata 9//
+  let counter9 = 100
+  while (counter9 >= 5) {
+    console.log(counter9)
+    counter9 -=5 
+  }
+
+  //kata 10//
+  let counter10 = 100
+  while (counter10 >= 1) {
+    let isSquare = Number.isInteger(Math.sqrt(counter10));
+    if (isSquare) {
+    console.log(counter10)
+    }
+    counter10 -= 1 
+  }
